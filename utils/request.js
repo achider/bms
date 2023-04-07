3eada51291115c17b14c46d56151fec7dccde87e
achider <1511269255@qq.com>
2023-04-07 20:33:06 +0800

    1.0.0

--- utils/request.js
+++ utils/request.js
@@ -2,7 +2,7 @@ export default (url,data={},method='GET')=>{
 	return new Promise((resolve,reject)=>{
 		uni.request({
 			url:'http://localhost'+url,
-			// url:'http://124.71.140.250:80'+url,
+			// url:'http://124.71.140.250'+url,
 			data,
 			method,
 			success:(res)=> {
